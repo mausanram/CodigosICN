1d9c8716e0c51b9358bcceb3c9329e8a57cf9e13
Mauricio Sánchez <mauricio06@ciecias.unam.mx>
2023-10-04 15:47:22 -0600

    Se corrigió la ecuacion de Bethe-Bloch y se agreggó al filtro del espectro

--- Catalogo_Eventos/EspectroEnergias.py
+++ Catalogo_Eventos/EspectroEnergias.py
@@ -11,10 +11,16 @@ import skimage as sk
 import datetime
 import pickle
 
-ratio_eV = 3.7
+ratio_keV = 0.0037
+CCD_depth = 725 #micras
+px_to_cm = 0.0015
+px_to_micras = 15
+micra_to_cm = 1 / 10000
+DeltaEL_range = 85
 Solidit = 0.7
 Elip = 4.5
 numero_bins = 5000
+DeltaEL_range_min, DeltaEL_range_max = 0.9, 3.55
 
 def gaussian(x, a, mean, sigma):
     return a * np.exp(-((x - mean)**2 / (2 * sigma**2)))
@@ -28,12 +34,15 @@ def Landau(x,a, MP,xi):
 
 def main(argObj):
     expgain = [227, 220.4, 94.72, 197.7]
+    list_All = []
     list_EventCharge_AllExtensions=[]
     list_EventosRectos = []
     list_EventosCirc = []
     list_totalEvents = []
+    list_DeltaEL = []
     Inicio = datetime.datetime.now()
     num_images =  'Imágenes Analizadas: ' +  str(len(argObj))
+    num_EventosRectos = 0
     
     print('Hora de inicio del cálculo: ', Inicio)
     for img in argObj:
@@ -54,7 +63,7 @@ def main(argObj):
 
             offset = bins_edges[np.argmax(hist)]
             dataP = data-offset
-            dataCal = (ratio_eV * dataP)/expgain[extension] ## En eV  
+            dataCal = (ratio_keV * dataP)/expgain[extension] ## En keV  
             
             del hist
             del data
@@ -120,58 +129,43 @@ def main(argObj):
                 rM = prop[event-1].axis_major_length
                 rm = prop[event-1].axis_minor_length
                 Solidity = prop[event-1].solidity
-                minr, minc, maxr, maxc = prop[event-1].bbox
+
+                miny, minx, maxy, maxx = prop[event-1].bbox
+                Longitud_y, Longitud_x = maxy - miny , maxx - minx # px
+                Diagonal_lenght= np.sqrt(Longitud_x**2 + Longitud_y**2) - np.sqrt(2) # px
+                Delta_L = np.sqrt( (Diagonal_lenght * px_to_micras)**2 + (CCD_depth)**2) # micras
 
                 if rM == 0 or rm == 0:
                     continue 
 
-                elif maxc - minc <= 3:
+                elif maxx - minx <= 3:
                     continue
 
                 elif not Barycentercharge:
                     continue
                 
-                elif differval < MeanValue_Event + 100:
+                elif differval < MeanValue_Event: #keV
                     continue
                 
                 elif  Solidity < Solidit: ## Ver el artículo: Recognition and classification of the cosmic-ray events in images captured by CMOS/CCD cameras
                     continue 
 
-                elif  rM > Elip * rm: ## Eventos Rectos
-                    list_EventosRectos.append(1)
-                    # charge = 0
-                    # for element in data_maskEvent.data.flatten():
-                    #     if element >= valor_promedio_fondo:
-                    #         charge = charge + element
+                elif  rM/rm > Elip: ## Eventos Rectos
                     charge = data_maskEvent.sum()
-                    # list_charge.append(charge)
-                    list_EventCharge_AllExtensions.append(charge)
+                    DeltaEL = charge / (Delta_L * micra_to_cm) # keV/cm
+                    list_DeltaEL.append(DeltaEL)
 
-                    del data_maskEvent
-                    del Barycentercharge
-                    del charge
+                    if DeltaEL_range_min < DeltaEL < DeltaEL_range_max:
+                        num_EventosRectos = num_EventosRectos + 1
+                        list_EventCharge_AllExtensions.append(charge)
 
-                # elif   0.9 < rM/rm < Elip  : ## Eventos Circulares ???
-                #     charge = 0
-                #     # for element in data_maskEvent.data.flatten():
-                #     #     if element >= valor_promedio_fondo:
-                #     #         charge = charge + element
-                #     charge = data_maskEvent.sum()
-                #     # list_charge.append(charge)
-                #     # charge = ratio_eV * charge
-                #     list_EventCharge_AllExtensions.append(charge)
-                #     list_EventosCirc.append(0)
-
-                #     del data_maskEvent
-                #     del Barycentercharge
-                #     del charge
-
-                
-
-            # print(list_eventos_rectos)
+                del data_maskEvent
+                del Barycentercharge
 
         del hdu_list            
     
+    list_All = [list_EventCharge_AllExtensions, list_DeltaEL]
+
     total_events = sum(list_totalEvents)
     Final = datetime.datetime.now()
 
@@ -179,51 +173,45 @@ def main(argObj):
     print('Tiempo de cálculo: ', Final-Inicio)
     print(num_images)
     Eventos_Totales = 'Eventos Detectados en Total: ' +  str(total_events)
-    eventos_rectos = 'Muones Rectos Detectados: ' + str(len(list_EventosRectos))
-    eventos_circulares = 'Muones Circulares Detectados: ' + str(len(list_EventosCirc))
+    eventos_rectos = 'Muones Rectos Detectados: ' + str(num_EventosRectos)
+    # eventos_circulares = 'Muones Circulares Detectados: ' + str(len(list_EventosCirc))
     # print('Número de elementos de la lista "list_EventCharge_AllExtensions": ', len(list_EventCharge_AllExtensions))
     # print('elementos de la lista "list_EventCharge_AllExtensions":', list_EventCharge_AllExtensions)
     print(Eventos_Totales)
     print(eventos_rectos)
-    # print('Eventos Circulares: ', len(list_EventosCirc))
-    all_lists = [list_EventCharge_AllExtensions, total_events, len(list_EventosRectos)]
+    # print(eventos_circulares)
 
     fig, axs = plt.subplots(1,1)
-    fig.canvas.manager.set_window_title('histogram_Imgs_'+str(len(argObj))+'_Sol_'+str(Solidit)+'_Elip_'+str(Elip)+'.pkl')
+    fig.canvas.manager.set_window_title('histogram_Imgs_'+str(len(argObj))+'_Sol_'+str(Solidit)+'_Elip_'+str(Elip))
     
     fig.suptitle('Espectro de Energía de Muones')
+
     bin_heights, bin_borders, _ = axs.hist(list_EventCharge_AllExtensions, bins = numero_bins, label= num_images + '\n' + eventos_rectos) 
+    # axs.hist(list_EventCharge_AllExtensions, bins = numero_bins, label= num_images + '\n' + eventos_rectos) 
     bin_centers = bin_borders[:-1] + np.diff(bin_borders) / 2 
 
-    # bin_centers=np.zeros(len(bin_heights), dtype=float)
-    # offset_fit = bin_borders[np.argmax(bin_heights)]
-    # for p in range(len(bin_heights)):
-    #     bin_centers[p]=(bin_borders[p+1]+bin_borders[p])/2
 
-    # xmin_fit, xmax_fit = offset_fit-(10*expgain[extension-1])/math.sqrt(nsamp), offset_fit+(10*expgain[extension-1])/math.sqrt(nsamp)			# Define fit range
-    # bin_heights = bin_heights[(bin_centers>xmin_fit) & (bin_centers<xmax_fit)]
-    # bin_centers = bin_centers[(bin_centers>xmin_fit) & (bin_centers<xmax_fit)]
+    # # xmin_fit, xmax_fit = offset_fit-(10*expgain[extension-1])/math.sqrt(nsamp), offset_fit+(10*expgain[extension-1])/math.sqrt(nsamp)			# Define fit range
+    # # xmin_fit, xmax_fit = min(list_EventCharge_AllExtensions), max(list_EventCharge_AllExtensions)
+    # # bin_heights = bin_heights[(bin_centers>xmin_fit) & (bin_centers<xmax_fit)]
+    # # bin_centers = bin_centers[(bin_centers>xmin_fit) & (bin_centers<xmax_fit)]
 
-    popt, _ = curve_fit(Landau, bin_centers, bin_heights, p0=[50,10000,1000])
-    print('Los parámetros del ajuste son: Altura: ', popt[0], ' EMP: ', popt[1], r' $\xi$: ',popt[2])
-    strAjuste = 'Ajuste: EMP: ' + str(popt[1]) + r' $\xi$: ' + str(popt[2])
-    x_interval_for_fit = np.linspace(bin_borders[0], bin_borders[-1], 10000)
-    axs.plot(x_interval_for_fit, Landau(x_interval_for_fit,*popt), label=strAjuste)
+
+    # popt, _ = curve_fit(Landau, bin_centers, bin_heights) # p0=[50,10000,1000])
+    # print('Los parámetros del ajuste son: Altura: ', popt[0], ' EMP: ', popt[1], r' $\xi$: ',popt[2])
+    # strAjuste = 'Ajuste: EMP: ' + str(popt[1]) + r' $\xi$: ' + str(popt[2])
+    # x_interval_for_fit = np.linspace(bin_borders[0], bin_borders[-1], 10000)
+    # axs.plot(x_interval_for_fit, Landau(x_interval_for_fit,*popt), label=strAjuste)
 
     axs.legend(loc="upper right") 
-    axs.set_xlabel(r'eV')
-    axs.set_ylabel('Cuentas') 
-    axs.set_xlim([0, 400000])  
+    axs.set_xlabel(r'Energy (keV)')
+    axs.set_ylabel('Events') 
+    # axs.set_xlim([0, 400000])  
 
-    ## Guarda la Figura ya hecha
-    # file_object_histogram = open('histogram_Imgs_'+str(len(argObj))+'_Sol_'+str(Solidit)+'_Elip_'+str(Elip)+'.pkl', 'wb')
-    # pickle.dump(fig, file_object_histogram)
-    # file_object_histogram.close()
-
-    ## Guarda los datos para hacer la figura.
-    file_raw_data = open('datahistogram_Imgs_'+str(len(argObj))+'_Sol_'+str(Solidit)+'_Elip_'+str(Elip)+'.pkl', 'wb')
-    pickle.dump(all_lists, file_raw_data)
-    file_raw_data.close()
+    file_object_histogram = open('data_histogram_Imgs_'+str(len(argObj))+'_Sol_'+str(Solidit)+'_Elip_'+str(Elip)+'_DeltaDL_' + \
+                                str(DeltaEL_range_min) + str(DeltaEL_range_max) +  '.pkl', 'wb')
+    pickle.dump(list_All, file_object_histogram)
+    file_object_histogram.close()
 
     plt.show() 
 
@@ -232,3 +220,4 @@ if __name__ == "__main__":
     argObj = sys.argv[1:]
     exitcode = main(argObj)
     exit(code = exitcode)
+
