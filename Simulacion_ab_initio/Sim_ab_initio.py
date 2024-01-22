f1cae288208bff8f187577b56998282c873e7338
Mauricio Sánchez <mauricio06@ciecias.unam.mx>
2024-01-22 15:50:14 -0600

    Se actualizó la simulación de muones y otros programas

--- Simulacion_ab_initio/Sim_ab_initio.py
+++ Simulacion_ab_initio/Sim_ab_initio.py
@@ -10,6 +10,7 @@ from mpl_toolkits.mplot3d import Axes3D
 Phi = np.arange(0, 2 * np.pi, 0.001)
 Radio = 100
 Theta = np.arange(0, np.pi/2, 0.001)    ### Semi-esfera de radio 100 unidades
+number_of_points_per_plane = 100
 
 long_x = np.arange(0, 10, 0.001)
 long_y = np.arange(0, 10, 0.001)
@@ -60,8 +61,10 @@ for i in np.arange(0,len(Theta)):
     # print(len(normal_Vec))
     Vectors.append(normal_Vec)
 
-    plane = Plane(normal_vec = Vec, d_origin = Radio, ref_point = Point, radius = 100.0)   ## Se crea el plano sobre la esfera
-    for i in np.arange(0,15):
+    plane = Plane(normal_vec = Vec, d_origin = Radio, ref_point = Point, radius = 60)   ## Se crea el plano sobre la esfera
+                                                                                        ## El término 'radius' es el que maneja el tamaño del plano.
+    
+    for i in np.arange(0,number_of_points_per_plane):
         random_plane_point = plane.create_random_points(1)   ## Selección aleatoria de un punto sobre el plano
         list_random_plane_point.append(random_plane_point)
 
@@ -73,16 +76,17 @@ random_phi_array = np.array(list_random_phi)
 # len(Random_th)
 # Random_th
 
-# fig, axs = plt.subplots(figsize=[7,5])
 
+## Grafica la distribución angular theta ###
+fig, axs = plt.subplots(figsize=[7,5])
 # # Theta
-# axs.plot(Theta, 70 * Theta_true)
+axs.plot(Theta, 70 * Theta_true)
 # # plt.plot(Theta_true, Theta)
 # # plt.plot(list_random_th, Theta, '.')
 # # plt.plot(Theta_true, list_random_th, '.')
-
-# axs.hist(random_th_array, bins = 110)
-# fig.suptitle(r'Distribución angular $\theta$', y = 0.95, size = 20)
+axs.hist(random_th_array, bins = 110)
+fig.suptitle(r'Distribución angular $\theta$', y = 0.95, size = 20)
+plt.show()
 
 
 ## Phi 
@@ -91,33 +95,32 @@ print('Número de planos: ', len(Theta))
 print('Número de puntos por plano: ', len(list_points_per_plane[0]))
 print('Número total de eventos simulados: ', len(Theta) * len(list_points_per_plane[0]))
 
-# plt.show()
 
-
+#### Se crea la figura en 3D
 fig = plt.figure()
-# Creamos el plano 3D
 ax1 = fig.add_subplot(111, projection='3d')
 Ax = Axes3D(fig)
-plano = 1
+plano = 0
 
 theta = np.arange(0, 2 * np.pi, 0.01)
 phi = np.arange(0, np.pi/2, 0.01)
 theta, phi = np.meshgrid(theta, phi)
 
-# Definimos los datos de prueba
+x_s = Radio * np.sin(phi) * np.cos(theta)
+y_s = Radio * np.sin(phi) * np.sin(theta)
+z_s = Radio * np.cos(phi)
+
+# Agrega la semi-esfera
+ax1.plot_surface(x_s, y_s, z_s, antialiased=False)
+
+
+# Toma las coordenadas de cada punto de un plano y las agrega a la figura
 for i in np.arange(0, len(list_points_per_plane[0])):
     x = list_points_per_plane[plano][i][0][0]
     y = list_points_per_plane[plano][i][0][1]
     z = list_points_per_plane[plano][i][0][2]
     # print(list_points_per_plane[0][i][0])
-    Ax.scatter(x, y, z, c='k', marker='o')
-
-x_s = Radio * np.sin(phi) * np.cos(theta)
-y_s = Radio * np.sin(phi) * np.sin(theta)
-z_s = Radio * np.cos(phi)
-
-# Agregamos los puntos en el plano 3D
-Ax.plot_surface(x_s, y_s, z_s)
+    ax1.scatter(x, y, z, c='k', marker='o')
 
 # Mostramos el gráfico
 plt.show()
