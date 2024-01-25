e55d1a31589c00771eb9464e7635026163a6dda9
Mauricio Sanchez <mauriciosr0629@gmail.com>
2024-01-24 21:36:14 -0600

    Se agregó un programa con las funciones que se ocupan para la simulación además de agregar los cambios que el doc Alexis pidió en los puntos aleatorios y la energía

--- Simulacion_ab_initio/Sim_ab_initio.py
+++ Simulacion_ab_initio/Sim_ab_initio.py
@@ -1,8 +1,10 @@
 import numpy as np
+import mpmath as mp
 import random as rand
 import pandas as pd
 import matplotlib.pyplot as plt
-from random_geometry_points.plane import Plane
+from random_geometry_points.plane import Plane   ### PAra instalar utilizar "pip install random-geometry-points"
+from funciones_Sim_ab_initio import *
 
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -10,32 +12,20 @@ from mpl_toolkits.mplot3d import Axes3D
 Phi = np.arange(0, 2 * np.pi, 0.001)
 Radio = 100
 Theta = np.arange(0, np.pi/2, 0.001)    ### Semi-esfera de radio 100 unidades
-number_of_points_per_plane = 100
 
-long_x = np.arange(0, 10, 0.001)
-long_y = np.arange(0, 10, 0.001)
-# long_z = np.arange(0, 10, 0.001)
+long_a = np.arange(0, 10, 0.001)
+long_b = np.arange(0, 10, 0.001)
 
-def dis_probability(theta, I_0):
-    return I_0 * np.cos(theta)
-
-def dis_angular(theta): ## Distribucion angular
-    return 1 * np.cos(theta)**2 * np.sin(theta)
-
-def dis_energy(theta): ### Poner la distribución de energías en función del angulo theta
-    return False
 
 Theta_true = dis_angular(Theta) ## Distribución angular theta real.
 
-##### Coordenadas Cartesianas Unitarias######
-def coord_cartesian(Thet, Phi):
-    coord_X = np.sin(Thet) * np.cos(Phi)
-    coord_Y = np.sin(Thet) * np.sin(Phi)
-    coord_Z = np.cos(Thet)
-
-    Vec_sph = (float(coord_X), float(coord_Y), float(coord_Z))
-    return Vec_sph 
-
+list_random_th = []
+list_random_phi = []
+list_random_a = []
+list_random_b = []
+list_points_per_plane = []
+Vectors = []
+Points = []
 
 list_random_th = []
 list_random_phi = []
@@ -61,11 +51,22 @@ for i in np.arange(0,len(Theta)):
     # print(len(normal_Vec))
     Vectors.append(normal_Vec)
 
-    plane = Plane(normal_vec = Vec, d_origin = Radio, ref_point = Point, radius = 60)   ## Se crea el plano sobre la esfera
-                                                                                        ## El término 'radius' es el que maneja el tamaño del plano.
-    
-    for i in np.arange(0,number_of_points_per_plane):
-        random_plane_point = plane.create_random_points(1)   ## Selección aleatoria de un punto sobre el plano
+    # plane = Plane(normal_vec = Vec, d_origin = Radio, ref_point = Point, radius = 10.0)   ## Se crea el plano sobre la esfera
+    vec_thet = [np.cos(Random_th) * np.cos(Random_phi), np.cos(Random_th) * np.sin(Random_phi), np.sin(Random_th)]
+    vec_phi = [-np.sin(Random_phi), np.cos(Random_phi), 0]
+
+    for i in np.arange(0,100):
+        # random_plane_point = plane.create_random_points(1)   ## Selección aleatoria de un punto sobre el plano
+        # list_random_plane_point.append(random_plane_point)
+        random_a = rand.choice(long_a)
+        list_random_a.append(random_a)
+
+        random_b = rand.choice(long_b)
+        list_random_b.append(random_b)
+
+        P_vector = [random_a * vec_thet[0] + random_b * vec_phi[0], random_a * vec_thet[1] + random_b * vec_phi[1], random_a * vec_thet[2] + random_b * vec_phi[2]]
+
+        random_plane_point = [Point[0] + P_vector[0], Point[1] + P_vector[1], Point[2] + P_vector[2]]
         list_random_plane_point.append(random_plane_point)
 
     list_points_per_plane.append(list_random_plane_point)
@@ -76,16 +77,22 @@ random_phi_array = np.array(list_random_phi)
 # len(Random_th)
 # Random_th
 
-
 ## Grafica la distribución angular theta ###
+# fig, axs = plt.subplots(figsize=[7,5])
+# # # Theta
+# axs.plot(Theta, 70 * Theta_true)
+# # # plt.plot(Theta_true, Theta)
+# # # plt.plot(list_random_th, Theta, '.')
+# # # plt.plot(Theta_true, list_random_th, '.')
+# axs.hist(random_th_array, bins = 110)
+# fig.suptitle(r'Distribución angular $\theta$', y = 0.95, size = 20)
+# plt.show()
+
+## Gráfica de distribución de los parámetros a y b ##
 fig, axs = plt.subplots(figsize=[7,5])
-# # Theta
-axs.plot(Theta, 70 * Theta_true)
-# # plt.plot(Theta_true, Theta)
-# # plt.plot(list_random_th, Theta, '.')
-# # plt.plot(Theta_true, list_random_th, '.')
-axs.hist(random_th_array, bins = 110)
-fig.suptitle(r'Distribución angular $\theta$', y = 0.95, size = 20)
+axs.hist(list_random_a, bins='auto')
+axs.hist(list_random_b, bins = 'auto')
+fig.suptitle(r'Distribución angular parámetros $a$ y $b$', y = 0.95, size = 20)
 plt.show()
 
 
@@ -111,16 +118,18 @@ y_s = Radio * np.sin(phi) * np.sin(theta)
 z_s = Radio * np.cos(phi)
 
 # Agrega la semi-esfera
-ax1.plot_surface(x_s, y_s, z_s, antialiased=False)
+# ax1.plot_surface(x_s, y_s, z_s, antialiased=False)
+# Ax.plot_surface(x_s, y_s, z_s, antialiased=False)
 
 
 # Toma las coordenadas de cada punto de un plano y las agrega a la figura
 for i in np.arange(0, len(list_points_per_plane[0])):
-    x = list_points_per_plane[plano][i][0][0]
-    y = list_points_per_plane[plano][i][0][1]
-    z = list_points_per_plane[plano][i][0][2]
+    x = list_points_per_plane[plano][i][0]
+    y = list_points_per_plane[plano][i][1]
+    z = list_points_per_plane[plano][i][2]
     # print(list_points_per_plane[0][i][0])
-    ax1.scatter(x, y, z, c='k', marker='o')
+    # ax1.scatter(x, y, z, c='k', marker='o')
+    Ax.scatter(x, y, z, c='k', marker='o')
 
 # Mostramos el gráfico
-plt.show()
+# plt.show()
