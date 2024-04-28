cadd051dc0c72a82aac0068f45a46c63de46919a
Mauricio Sanchez <mauriciosr0629@gmail.com>
2024-04-27 22:14:40 -0600

    Se reescribió el catálogo de muones de NSAM1

--- Catalogo_Eventos/Espectro_Muones.py
+++ Catalogo_Eventos/Espectro_Muones.py
@@ -1,4 +1,5 @@
 # from functions_py import math
+from functions_MuonsNSAMP1 import *
 import math
 from astropy.io import fits
 import scipy.ndimage as ndimage
@@ -22,10 +23,11 @@ px_to_micras = 15
 micra_to_cm = 1 / 10000
 DeltaEL_range = 85
 Solidit = 0.7
-Elip = 5.5
+Elip = 3.5
 numero_bins = 5000
 DeltaEL_range_min, DeltaEL_range_max = 0.9, 3.55
 
+units = 1
 n_sigmas = 6
 
 def gaussian(x, a, mean, sigma):
@@ -43,12 +45,12 @@ def main(argObj):
 
     # expgain = [110.78158608889959, 144.4661840232508, 100,  63.730700893071976] ## 
 
+    list_totalEvents = []
 
     list_EventCharge_extension_2 =[]
     list_EventCharge_extension_1 = []
     list_EventCharge_extension_4 = []
 
-    list_totalEvents = []
     list_DeltaEL_extension_2 = []
     list_DeltaEL_extension_1 = []
     list_DeltaEL_extension_4 = []
@@ -57,13 +59,16 @@ def main(argObj):
     list_DeltaL_extension_1 = []
     list_DeltaL_extension_4 = []
 
+    list_theta_extension_2 = []
+    list_theta_extension_1 = []
+    list_theta_extension_4 = []
+
 
     total_images = len(argObj)
     image_in_bucle = 0
 
     Inicio = datetime.datetime.now()
     num_images =  'Imágenes Analizadas: ' +  str(total_images)
-    num_muons = 0
     
     print('Hora de inicio del cálculo: ', Inicio)
     for img in argObj:
@@ -76,141 +81,73 @@ def main(argObj):
             # extension = 1
 
             data = hdu_list[extension].data[:,:550]
-            header = hdu_list[extension].header
             oScan = hdu_list[extension].data[:,550:]
-            nsamp = float(header['NSAMP'])
+            # nsamp = float(header['NSAMP'])
 
-            del header
+            # oScan_fit(extensión, active_area, oScan, Bins, make_figure_flag = False):
 
-            hist , bins_edges = np.histogram(oScan.flatten(), bins = numero_bins) #'auto'
-
-            offset = bins_edges[np.argmax(hist)]
-
-            ## Aplanamos el Overscan
-            Overscan_plane = oScan - offset
-
-            del oScan
-
-            dataP = data - offset
-            dataCal = ((dataP)/expgain[extension]) * ratio_keV ## En KeV
-            # dataCal = dataP ## En ADUs
-            
-            del hist
-            del data
-            del dataP
-            
-            bin_heights, bin_borders = np.histogram(Overscan_plane.flatten(), bins= numero_bins, range=(-2000, 2000)) #'auto'
-            bin_centers=np.zeros(len(bin_heights), dtype=float)
-            offset_fit = bin_borders[np.argmax(bin_heights)]
-            for p in range(len(bin_heights)):
-                bin_centers[p]=(bin_borders[p+1]+bin_borders[p])/2
-
-            xmin_fit, xmax_fit = -abs(offset), abs(offset)			# Define fit range
-            bin_heights = bin_heights[(bin_centers>xmin_fit) & (bin_centers<xmax_fit)]
-            bin_centers = bin_centers[(bin_centers>xmin_fit) & (bin_centers<xmax_fit)]
-
-            del nsamp
-
-            try:
-                popt,_ = curve_fit(gaussian, bin_centers, bin_heights, maxfev=100000, p0 = [1,1,100])		# Fit histogram with gaussian
-
-                sig_ADUs = popt[2]
-                # Gain = popt[2]
-
-                sig_KeV = abs((ratio_keV * sig_ADUs) / expgain[extension-1])
-                fondo_value = n_sigmas * sig_KeV
+            try : 
+                dict_popt = oScan_fit(extensión=extension, active_area=data, oScan=oScan, Bins=numero_bins, make_figure_flag=False)
 
             except:
-                print('Fit error in image ' + str(img))
+                print('Fit error in extension ' + str(extensión) + 'of image ' + str(img))
                 continue
 
-            del bin_heights
-            del bin_centers
-            del offset
-            del xmin_fit
-            del xmax_fit
+            sig_ADUs = dict_popt['sigma']
+            sig_electrons = (sig_ADUs) / expgain[extension-1]
+            sig_KeV = sig_electrons * ratio_keV
+            Offset = dict_popt['Offset']
+
+            dataCal = data_calibrated(active_area=data, extension=extension, list_gain=expgain, ratio_keV=ratio_keV, unidades= units, offset=Offset)
+
+            if units == 0:
+                fondo_value = n_sigmas * sig_ADUs
+            elif units == 1:
+                fondo_value = n_sigmas * sig_electrons
+            elif units == 2:
+                fondo_value = n_sigmas * sig_KeV
+            
+            del oScan
+            # del bin_heights
+            # del bin_centers
+            # del offset
+            # del xmin_fit
+            # del xmax_fit
 
             # label, n_events = ndimage.label(dataCal>6*abs(popt[2]),structure=[[1,1,1],[1,1,1],[1,1,1]])
             label_img, n_events = sk.measure.label(dataCal > fondo_value, connectivity=2, return_num=True)
             prop = sk.measure.regionprops(label_img, dataCal)
+
             list_totalEvents.append(n_events)
             # print(nlabels_img)
             # list_labels.append(label_img)
             # list_EventsNumber.append(n_events)
             
-            ## Obteniendo el valor promedio del fondo
-            fondo_mask = np.invert(label_img == 0)
-            fondo = ma.masked_array(dataCal,fondo_mask)
-            valor_promedio_fondo = fondo.data.mean()
+            DeltaL, DeltaEL, list_charge, _, list_theta = muon_filter(dataCal=dataCal, label_img=label_img, nlabels_img=n_events, 
+                                                                    prop=prop, Solidit=Solidit, Elipticity=Elip)
 
-            for event in range(1,n_events):
-                mask = np.invert(label_img == event)
-                loc = ndimage.find_objects(label_img == event)[0]
-                data_maskEvent = ma.masked_array(dataCal[loc[0].start:loc[0].stop, loc[1].start:loc[1].stop],
-                                            mask[loc[0].start:loc[0].stop, loc[1].start:loc[1].stop])
-                
-                # del dataCal
-
-                coordX_centerCharge = round(ndimage.center_of_mass(data_maskEvent)[1])
-                coordY_centerCharge = round(ndimage.center_of_mass(data_maskEvent)[0])
-
-                MeanValue_Event = data_maskEvent.mean()
-                MinValue_Event = data_maskEvent.min()
-
-                Barycentercharge = data_maskEvent[coordY_centerCharge, coordX_centerCharge]
-
-                try:
-                    differval = abs(Barycentercharge - MinValue_Event) 
-                except:
-                    differval = 0 
-
-                rM = prop[event-1].axis_major_length
-                rm = prop[event-1].axis_minor_length
-                Solidity = prop[event-1].solidity
-
-                miny, minx, maxy, maxx = prop[event-1].bbox
-                Longitud_y, Longitud_x = maxy - miny , maxx - minx # px
-                Diagonal_lenght= np.sqrt(Longitud_x**2 + Longitud_y**2) - np.sqrt(2) # px
-                Delta_L = np.sqrt( (Diagonal_lenght * px_to_micras)**2 + (CCD_depth)**2) * micra_to_cm # cm
-
-                if rM == 0 or rm == 0:
-                    continue 
-
-                elif maxx - minx <= 3:
-                    continue
-
-                elif not Barycentercharge:
-                    continue
-                
-                # if differval < MeanValue_Event: #ADUs
-                #     continue
-                
-                elif  Solidity < Solidit: ## Ver el artículo: Recognition and classification of the cosmic-ray events in images captured by CMOS/CCD cameras
-                    continue 
-
-                if  rM >= Elip * rm: ## Muones
-                    charge = data_maskEvent.sum() / 1000 ## EN MeV
-                    DeltaEL = charge / (Delta_L * micra_to_cm) # MeV/cm
-                    # DeltaEL = charge / (Delta_L) # ADUs/cm
-                    num_muons = num_muons + 1
-
-                    if extension == 0:
-                        list_DeltaEL_extension_1.append(DeltaEL)
-                        list_EventCharge_extension_1.append(charge)
-                        list_DeltaL_extension_1.append(Delta_L)
-
-                    elif extension == 1:
-                        list_DeltaEL_extension_2.append(DeltaEL)
-                        list_EventCharge_extension_2.append(charge)
-                        list_DeltaL_extension_2.append(Delta_L)
-
-                    elif extension == 3:
-                        list_DeltaEL_extension_4.append(DeltaEL)
-                        list_EventCharge_extension_4.append(charge)
-                        list_DeltaL_extension_4.append(Delta_L)
-
-                del data_maskEvent
-                del Barycentercharge
+            if extension == 0: 
+                for index in np.arange(0, len(DeltaEL)):
+                    list_DeltaEL_extension_1.append(DeltaEL[index])
+                    list_EventCharge_extension_1.append(list_charge[index])
+                    list_DeltaL_extension_1.append(DeltaL[index])
+                    list_theta_extension_1.append(list_theta[index])
+                    
+            if extension == 1: 
+                for index in np.arange(0, len(DeltaEL)):
+                    list_DeltaEL_extension_2.append(DeltaEL[index])
+                    list_EventCharge_extension_2.append(list_charge[index])
+                    list_DeltaL_extension_2.append(DeltaL[index])
+                    list_theta_extension_2.append(list_theta[index])
+            
+            if extension == 3: 
+                for index in np.arange(0, len(DeltaEL)):
+                    list_DeltaEL_extension_4.append(DeltaEL[index])
+                    list_EventCharge_extension_4.append(list_charge[index])
+                    list_DeltaL_extension_4.append(DeltaL[index])
+                    list_theta_extension_4.append(list_theta[index])
+            # del data_maskEvent
+            # del Barycentercharge
 
         del hdu_list            
 
@@ -218,7 +155,9 @@ def main(argObj):
 
     # dict_to_save_pkl = {'Muons_Detected' : num_muons, 'charge' : list_EventCharge_AllExtensions, 'DeltaEL' : list_DeltaEL}
 
-    dict_to_save_pkl = {'Num_Images' : total_images , 'All_Muons_Detected' : num_muons, 'Energy_Units' : 'MeV',
+    num_muons = len(list_EventCharge_extension_1) + len(list_EventCharge_extension_2) + len(list_EventCharge_extension_4)
+
+    dict_to_save_pkl = {'Num_Images' : total_images , 'All_Muons_Detected' : num_muons, 'Energy_Units' : units,
                         'extension_1' : {'charge' : list_EventCharge_extension_1, 'deltaEL' : list_DeltaEL_extension_1, 'deltaL' : list_DeltaL_extension_1}, 
                         'extension_2' : {'charge' : list_EventCharge_extension_2, 'deltaEL' : list_DeltaEL_extension_2, 'deltaL' : list_DeltaL_extension_2},
                         'extension_4' : {'charge' : list_EventCharge_extension_4, 'deltaEL' : list_DeltaEL_extension_4, 'deltaL' : list_DeltaL_extension_4}}
@@ -241,7 +180,14 @@ def main(argObj):
     # print(eventos_circulares)  
 
     # file_name = 'dict_muons_Extensions_1_to_4_Imgs_' + str(len(argObj))+'_Sol_' + str(Solidit) + '_Elip_'+str(Elip) + '_ADUs__.pkl'
-    file_name = 'dict_muons_Extensions_1_to_4_Imgs_' + str(len(argObj))+'_Sol_' + str(Solidit) + '_Elip_'+str(Elip) + '_MeVs__.pkl'
+
+    if units == 0:
+        file_name = 'dict_muons_Extensions_1_to_4_Imgs_' + str(len(argObj))+'_Sol_' + str(Solidit) + '_Elip_'+str(Elip) + '_ADUs__.pkl'
+    elif units == 1:
+        file_name = 'dict_muons_Extensions_1_to_4_Imgs_' + str(len(argObj))+'_Sol_' + str(Solidit) + '_Elip_'+str(Elip) + '_electrons__.pkl'
+    elif units == 2:
+        file_name = 'dict_muons_Extensions_1_to_4_Imgs_' + str(len(argObj))+'_Sol_' + str(Solidit) + '_Elip_'+str(Elip) + '_KeV__.pkl'
+
     file_object_histogram = open(file_name, 'wb')
     pickle.dump(dict_to_save_pkl, file_object_histogram) ## Save the dictionary with all info 
     file_object_histogram.close()
