b23895fb6eb3123b569f4dc2b916c6a45a4a4b9a
Mauricio Sanchez <mauriciosr0629@gmail.com>
2024-09-22 16:29:33 -0600

    Se están haciendo pruebas en el programa del CLUSTER

--- Simulacion_ab_initio/funciones_Sim_ab_initio.py
+++ Simulacion_ab_initio/funciones_Sim_ab_initio.py
@@ -1010,8 +1010,12 @@ def muon_generator_CLUSTER(Energy, number_thet,Theta, Theta_true, Phi, Radio, nu
 
                 os.environ["DELTA_L"] = str(Delta_L)
 
+                print(os.environ["DELTA_L"])
+                print('Entrando al proceso de ROOT')
+                
                 new_env = subprocess.run(["root", "-l", "-b", "/home/icn/mausanram/Software/CodigosICN/Simulacion_ab_initio/LandauVavilov_Mau.C", "-q"], 
                                             capture_output=True)
+
 
                 # print('Energía de SMith-Duller: ', os.getenv("EN_SMITH"))
                 # print(new_env.stdout)
