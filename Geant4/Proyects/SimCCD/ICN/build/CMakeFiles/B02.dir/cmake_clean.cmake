837507bc3401f4b95b515c8ca0ce5407374d0b40
Mauricio Sánchez <mauricio06@ciecias.unam.mx>
2025-01-13 14:15:13 -0600

    S

---
+++ Geant4/Proyects/SimCCD/ICN/build/CMakeFiles/B02.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/B02"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/B02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
