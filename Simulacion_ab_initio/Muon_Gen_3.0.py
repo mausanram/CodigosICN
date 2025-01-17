447a8193c3a349bcd708d0d3a6f3b03b5277c684
Mauricio Sanchez <mauriciosr0629@gmail.com>
2025-01-16 20:51:16 -0600

    S

--- Simulacion_ab_initio/Muon_Gen_3.0.py
+++ Simulacion_ab_initio/Muon_Gen_3.0.py
@@ -68,7 +68,7 @@ def main():
     ### Número de muones a simular ### 
     number_thet = 1000     ## Valores de un ángulo Theta.
     n_muons = number_thet  ## Número total de muones que se simularán.
-    nmuons_perbucle = 200
+    nmuons_perbucle = 20000
 
     niterations = n_muons / nmuons_perbucle
 
