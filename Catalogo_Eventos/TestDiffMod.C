58747a81986fcc286849ba64069c58ffedea1b15
Mauricio Sanchez <mauriciosr0629@gmail.com>
2025-09-08 20:57:22 -0600

    08Sep2025

--- Catalogo_Eventos/TestDiffMod.C
+++ Catalogo_Eventos/TestDiffMod.C
@@ -1,9 +1,9 @@
 void TestDiffMod(){
 TChain *tree = new TChain("tree");
-tree->Add("tree_DiffusionMod_Ext1_Vert.root");
+// tree->Add("tree_DiffusionMod_Ext1_Vert.root");
 // tree->Add("tree_DiffusionMod_Ext1_Horz.root");
 // tree->Add("tree_DiffusionMod_Ext2_Vert.root");
-// tree->Add("tree_DiffusionMod_Ext2_horz.root");
+tree->Add("tree_DiffusionMod_Ext2_Horz.root");
 
 float sprd_val;
 float deep_val;
@@ -33,7 +33,7 @@ diff_data->GetXaxis()->SetRangeUser(0, 730);
 diff_data->GetYaxis()->SetRangeUser(0, 1.3);
 diff_data->GetXaxis()->SetTitle("Deep (mu m)");
 diff_data->GetYaxis()->SetTitle("Spread (px)");
-diff_data->SetTitle("Diffusion Model (Ext1-Vert)");
+diff_data->SetTitle("Diffusion Model (Ext2-Vert)");
 
 TF1 *diff_curve = new TF1("diff_curve", "sqrt([0]* log(1 - [1]*x))/15", 0, 725);
 diff_curve->SetParameters(-300, 0.0004);
@@ -57,5 +57,43 @@ diff_curve->Draw("same");
 std::cout<<"Chi2: " << diff_curve->GetChisquare() << endl;
 std::cout<<"NDF: " << diff_curve->GetNDF() << endl;
 std::cout<<"Prob: " << diff_curve->GetProb() << endl;
+
+
+TCanvas *canv = new TCanvas("canv","", 2*700, 600);
+TF1 *diff_curve_0 = new TF1("diff_curve_0", "sqrt([0]* log(1 - [1]*x))/15", 0, 725);
+diff_curve_0->SetLineColor(1);
+diff_curve_0->GetXaxis()->SetTitle("Deep (mu m)");
+diff_curve_0->GetYaxis()->SetTitle("Spread (px)");
+diff_curve_0->SetTitle("Diffusion Model");
+
+TF1 *diff_curve_1 = new TF1("diff_curve_1", "sqrt([0]* log(1 - [1]*x))/15", 0, 725);
+diff_curve_1->SetLineColor(2);
+
+TF1 *diff_curve_2 = new TF1("diff_curve_2", "sqrt([0]* log(1 - [1]*x))/15", 0, 725);
+diff_curve_2->SetLineColor(4);
+
+TF1 *diff_curve_3 = new TF1("diff_curve_3", "sqrt([0]* log(1 - [1]*x))/15", 0, 725);
+diff_curve_3->SetLineColor(6);
+
+
+diff_curve_0->SetParameters(-306.329, 0.0007903);
+diff_curve_1->SetParameters(-844.967, 0.0003287);
+diff_curve_2->SetParameters(-332.143, 0.0007341);
+diff_curve_3->SetParameters(-487.071, 0.0004444);
+diff_curve_0->Draw();
+diff_curve_1->Draw("same");
+diff_curve_2->Draw("same");
+diff_curve_3->Draw("same");
+
+TLegend *leg = new TLegend(0.7, 0.8, 0.9, 0.9);
+// leg->SetTextAlign(11);
+// leg->SetFillStyle(0);
+leg->AddEntry(diff_curve_0, "Ext1-Vert", "l");
+leg->AddEntry(diff_curve_1, "Ext1-Horz", "l");
+leg->AddEntry(diff_curve_2, "Ext2-Vert", "l");
+leg->AddEntry(diff_curve_3, "Ext2-Horz", "l");
+leg->Draw();
+
+
 
 }
