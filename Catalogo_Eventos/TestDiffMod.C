5ec7b93b733ef662265b743cc883988dbc64a2c3
Mauricio Sanchez <mauriciosr0629@gmail.com>
2025-09-13 17:08:18 -0600

    M

--- Catalogo_Eventos/TestDiffMod.C
+++ Catalogo_Eventos/TestDiffMod.C
@@ -1,9 +1,11 @@
+#include <string>
+
 void TestDiffMod(){
 TChain *tree = new TChain("tree");
 // tree->Add("tree_DiffusionMod_Ext1_Vert.root");
 // tree->Add("tree_DiffusionMod_Ext1_Horz.root");
-// tree->Add("tree_DiffusionMod_Ext2_Vert.root");
-tree->Add("tree_DiffusionMod_Ext2_Horz.root");
+tree->Add("tree_DiffusionMod_Ext2_Vert.root");
+// tree->Add("tree_DiffusionMod_Ext2_Horz.root");
 
 float sprd_val;
 float deep_val;
@@ -31,9 +33,10 @@ TGraph *diff_data = new TGraph(nentries, arr_deep, arr_sprd);
 diff_data->SetMarkerStyle(2);
 diff_data->GetXaxis()->SetRangeUser(0, 730);
 diff_data->GetYaxis()->SetRangeUser(0, 1.3);
-diff_data->GetXaxis()->SetTitle("Deep (mu m)");
-diff_data->GetYaxis()->SetTitle("Spread (px)");
-diff_data->SetTitle("Diffusion Model (Ext2-Vert)");
+diff_data->GetXaxis()->SetTitle("Profundidad (#mu m)");
+diff_data->GetYaxis()->SetTitle("Anchura (px)");
+// diff_data->SetTitle("Modelo de Difusi#acute{o}n(Extensi#acute{o}n 1)");
+diff_data->SetTitle("Modelo de Difusi#acute{o}n(Extensi#acute{o}n 2)");
 
 TF1 *diff_curve = new TF1("diff_curve", "sqrt([0]* log(1 - [1]*x))/15", 0, 725);
 diff_curve->SetParameters(-300, 0.0004);
@@ -46,13 +49,13 @@ TGraph *frame = new TGraph(10, 0, 725);
 diff_data->Draw("AP");
 diff_curve->Draw("same");
 
-// TLegend *leg = new TLegend(0.7, 0.8, 0.9, 0.9);
-// // leg->SetTextAlign(11);
+TLegend *leg = new TLegend(0.7, 0.8, 0.9, 0.9);
+// leg->SetTextAlign(11);
 // leg->SetFillStyle(0);
-// leg->AddEntry(diff_curve, "Diff. Mod, fit: ", "l");
-// leg->AddEntry(line, "Grosor de la CCD: 0.0725 cm", "l");
-// // leg->AddEntry(line, "Grosor de la CCD: 0.068 cm", "l");
-// leg->Draw();
+// leg->AddEntry(diff_curve, "#sqrt{#alpha log(1 - #beta z)} fit: #alpha = -306.329 #mu m^{2}, #beta = 0.0007903 #mu m^{-1}", "l"); // Estension 1
+leg->AddEntry(diff_curve, "#sqrt{#alpha log(1 - #beta z)} fit: #alpha = -332.143 #mu m^{2}, #beta = 0.0007341 #mu m^{-1}", "l"); // Estension 2
+// leg->AddEntry(line, "Grosor de la CCD: 0.068 cm", "l");
+leg->Draw();
 
 std::cout<<"Chi2: " << diff_curve->GetChisquare() << endl;
 std::cout<<"NDF: " << diff_curve->GetNDF() << endl;
@@ -85,14 +88,14 @@ diff_curve_1->Draw("same");
 diff_curve_2->Draw("same");
 diff_curve_3->Draw("same");
 
-TLegend *leg = new TLegend(0.7, 0.8, 0.9, 0.9);
-// leg->SetTextAlign(11);
-// leg->SetFillStyle(0);
-leg->AddEntry(diff_curve_0, "Ext1-Vert", "l");
-leg->AddEntry(diff_curve_1, "Ext1-Horz", "l");
-leg->AddEntry(diff_curve_2, "Ext2-Vert", "l");
-leg->AddEntry(diff_curve_3, "Ext2-Horz", "l");
-leg->Draw();
+// TLegend *leg = new TLegend(0.7, 0.8, 0.9, 0.9);
+// // leg->SetTextAlign(11);
+// // leg->SetFillStyle(0);
+// leg->AddEntry(diff_curve_0, "", "l");
+// leg->AddEntry(diff_curve_1, "Ext1-Horz", "l");
+// leg->AddEntry(diff_curve_2, "Ext2-Vert", "l");
+// leg->AddEntry(diff_curve_3, "Ext2-Horz", "l");
+// leg->Draw();
 
 
 
